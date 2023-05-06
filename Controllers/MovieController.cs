3964b4ed8cfa2e435f0b39a03c5dd02e231c4d6c
Tyson Nguyen <yngu4822@gmail.com>
2023-05-06 15:12:41 +1000

    updated view Movie index and direct page

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -4,36 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace blackBox.Controllers
 {
     public class MovieController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movie
         public ActionResult Index()
         {
-            var movie = GetMovies();
+            var movie = _context.Movies.Include(c => c.Genre);
+
             return View(movie);
         }
 
         public ActionResult Direct(int? id)
         {
-            var movie = GetMovies().SingleOrDefault(param => param.Id == id);
+            var movie = _context.Movies.SingleOrDefault(param => param.Id == id);
             if(movie == null)
             {
                 return HttpNotFound("Selected movie Id not found in our server!");
             }
             return View(movie);
-        }
-
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-E"}
-            };
         }
     }
 }
