b985ff0d991bb6e047005c14b1983807f12100dc
Tyson Nguyen <yngu4822@gmail.com>
2023-05-29 13:52:37 +1000

    updated with DriverLiense domain model, needs view!

---
+++ Migrations/202305280628147_AddDrivingLicenseToApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿namespace blackBox.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDrivingLicenseToApplicationUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "DriverLicense", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "DriverLicense");
+        }
+    }
+}
