1f175cff018f3cca2dd08f97e1006b85c719caf0
Tyson Nguyen <yngu4822@gmail.com>
2023-06-02 13:02:08 +1000

    implemented output caching and glimpse for debugging and monitor performance

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace blackBox.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 120, Location = OutputCacheLocation.Server, VaryByParam ="*")]
         public ActionResult Index()
         {
             return View();
